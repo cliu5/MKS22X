4fd7debbc4d5c0f5de1c0d780891dc151c6207cf
cliu5 <cliu5@stuy.edu>
2018-02-05 13:52:28 -0500

    Update Recursion.java
    Did Second fib function.

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -11,3 +11,22 @@ public class Recursion{
     return fact(n-1)*n;
     }
     }
+//fib with helper//
+  public static int Helper( int n, int First, int Second){
+    if (n==0){
+      return First;
+    }
+    else{
+      return Helper(n-1,Second,First+Second);
+    }
+    //the first one is the number two before and the second one is the number one before//
+    //this took somethinking but you recursively go thru and store frist+second//
+    
+  public int fib(int n){
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	}
+	return Helper(n, 0, 1);
+    }
+
+   
