af9a4c57ff8d2e46bf1f438226001afd734e5583
cliu5 <cliu5@stuy.edu>
2018-02-05 13:45:28 -0500

    Update Recursion.java
    Finished factorial.

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,1 +1,13 @@
 
+public class Recursion{
+  public int fact(int n){
+  if (n < 0){
+      throw new IllegalArgumentException();
+    }
+    if(n==0){
+    return 1;
+    }
+    else{
+    return fact(n-1)*n;
+    }
+    }
