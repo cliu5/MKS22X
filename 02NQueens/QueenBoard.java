55104ed075afcad47fd110187a0ca15f082e936d
cliu5 <cliu5@stuy.edu>
2018-02-13 22:24:08 -0500

    Update QueenBoard.java

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -1,63 +1,133 @@
 public class QueenBoard{
     private int[][]board;
+    private int count;
     public QueenBoard(int size){
-	int[][] board= new int[size][size];
+	    if(size<0){
+		    throw new IllegalArgumentException();
+	    }
+	    
+	board= new int[size][size];
+	    this.size=size;
 	for(int i=0;i<board.size;i++){
 	    for(int j=0;j<board.size;j++){
 		board[i][j]=0;
 	    }
 	}
     }
-    private boolean addQueen(int r, int c){
-	board[r][c]=-1;
-	for (int i=0;i<board.size;i++){
-	    for(int j=0;j<board.size;j++){
-		if (i=r+1&&j=c+1){
-		    board[i][j]=board[i][j]+1
-			}
-	    }
-	}
-    }
-    private boolean removeQueen(int r, int c){
-	board[r][c]=0;
-    }
-      /**
-  *@return The output string formatted as follows:
-  *All numbers that represent queens are replaced with 'Q' 
-  *all others are displayed as underscores '_'
-  *There are spaces between each symbol:  
-  *Q _ _ _
-  *_ _ Q _
-  * etc.
-  */
-    public String toString(){
-	int[][]ShowBoard=new int[size][size];
 	
-	for(int i=0;i<board.size;i++){
-	    for(int j=0;j<board.size;j++){
-		if(board[i][j]==-1){
-		    ShowBoard[i][j]="Q";
+	//thanks to kristen at the dojo for helping me with understanding the algorithm and debugging addQueen & removeQueen//
+	
+    private boolean addQueen(int r, int c){
+	if(board[r][c]==0){
+	    board[r][c]=-1;
+	    for(int i=1 ; c+i<size; i++){
+		
+		board[r][c+i]+=1;
+			}
+	if(r+i<size){
+		//diagonals//
+		board[r+i][c+i]+=1;
+	}
+		if (r-i>=0){
+			board[r-i][c+i]+=1;
 		}
+	}
+	    return true; 
+    }
+	   return false;
+}
+    
+
+    private boolean removeQueen(int r, int c){
+	if(board[r][c]==0){
+		board[r][c]+=1;
+		for(int i=1;c+i<size;i++){
+			board[r][c+i]-=1;
+			if(r+i<size){
+				board[r+i][c+i]-=1;
+			}
+			if(r-i>=0){
+				board[r-i][c+i]-=1;
+			}
+		}
+		return true;
+	}
+	    return false;
+    }
+
+     
+
+    public String toString(){
+	    String ans="";
+	for(int i=0;i<size;i++){
+	    for(int j=0;j<size;j++){
+		    if(board[i][j]!=-1){
+			    ans+="_";
 		else{
-		    ShowBoard[i][j]="_";
+		    ans+="Q";
 		}
 	    }
 	}
+	    return ans;
     }
     
   
-  /**
-  *@return false when the board is not solveable and leaves the board filled with zeros; 
-
-  *        true when the board is solveable, and leaves the board in a solved state
-
-  *@throws IllegalStateException when the board starts with any non-zero value
-
-  */
+  
     public boolean solve(){
+    for(int i=0;i<size;i++){
+	    for(int j=0;j<size;j++){
+		    if(board[i][j]!=0){
+			    throw new IllegalStateException();
+		    }
+	    }
     }
-    public boolean solveHelper(){
+	    return solveHelper(0,0);
     }
+	    
+    public boolean solveHelper(int COLUMN, int QUEENCOUNT){
+	    if(QUEENCOUNT==SIZE){
+		    return true;
+	    }
+	    //once the amt of queens is equal to the size of the board u solved it thats the base case//
+	    for (int i=0;i<size; i++){
+		    //loop thru and check if you can add a queen//
+		    if(addQueen(i,COLUMN)){
+			    //if u can u add 1 to the column and move on//
+			    if(solveHelper(COLUMN+1)){
+				    return true;
+			    }
+			    // if you can't you remove first then back track and delete//
+			    removeQueen(i,COLUMN);
+		    }
+	    }
+	    return false;
+    }
+	    
+	    
+    
     public int countSolutions(){
+	    for(int i=0;i<size;i++){
+		    for(int j=0;j<size;j++):
+		    if(board[i][j]!=0){
+			    throw new IllegalStateException();
+		    }
+	    }
+	    return countSolutionsHelper(0);
     }
-}
+	    public int countSolutionsHelper(int COLUMN){
+		    int ans=0;
+		    if(COLUMN==size){
+			    return 1;
+		    }
+		    //get to end means 1 solution//
+		    for(int i=0;i<size;i++){
+			    if(addQueen(i,COLUMN)){
+				    ans+=countSolutionsHelper(COLUMN+1);
+				    //if u cant u backtrack and remove!!//
+				    removeQueen(i,COLUMN);
+			    }
+		    }
+		    return ans;
+	    }
+    }
+
