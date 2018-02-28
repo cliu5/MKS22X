279744bb07a1758927c68854539c70ddddcd43c9
cliu5 <cliu5@stuy.edu>
2018-02-27 23:33:56 -0500

    Update KnightBoard.java

--- 03KnightTour/KnightBoard.java
+++ 03KnightTour/KnightBoard.java
@@ -1,137 +1,119 @@
 public class KnightBoard(){
     private int[][]board;
+	private int[] ROWMOVES={2,2,1,-1,-2,-2,1,-1};
+	private int[] COLUMNMOVES={1,-1,2,2,1,-1,-2,-2};
+	
     public KnightBoard(int startingRows,int startingCols){
     if (startingRows<0||startingCols<0){
 	throw new IllegalArgumentException();
-    }
+    }    
     board= new int[startingRows][startingCols];
-    for (int i=0;i<startingRows;i++){
-	for (int j=0;j<startingCols;j++){
-	    board[i][j]=="0";
-	}
-    }
     }
 
 public String toString() {
-    ans=""
-     for (int i=0;i<startingRows;i++){
-	for (int j=0;j<startingCols;j++){
-	    if(board[i][j]=="0"){
-		ans+="_";
-	    }
-	}
+    String ans=""
+     for (int ROW=0;ROW<board.length;ROW++){
+
+     for (int COLUMN=0;COLUMN<board[ROW].length;COLUMN++){
+	     if(board.length*board[row].length<10){
+		     if(board[ROW][COLUMN]==0){
+			      ans+="_";
+		     }
+		     else{
+			      ans +=" " + board[ROW][COLUMN]+ " " ;
+		     }
+	     }
+	     else{
+		     if(board[ROW][COLUMN]<10){
+			     if(board[ROW][COLUMN]==0){
+				     ans+="_";
+			     }
+			     else{
+				     ans+=" " + board[ROW][COLUMN]+" ";
+			     }
+		     }
+		     else{
+			     ans+=board[ROW][COL]+ " ";
+		     }
+	     }
      }
+ans+= "\n";
+     }
+	return ans;
 }
 
-    
-
-
-
-  public boolean solve(int startingRow, int startingCol){
-    for (int i = 0; i < board.length; i++){
-      for (int j = 0; j < board[i].length; j++){
-        if (board[i][j] != 0){
-          throw new IllegalStateException();
-        }
-      }
-    }
-
-    if (startingRow < 0 || startingCol < 0){
-      throw new IllegalArgumentException();
-    }
-    return solveHelper(startingRow, startingCol, 1);
-  }
-
-  private boolean solveH(int row, int col, int level){
-    int[][] coordinates = {{2,1},{1,2},{-2,1},{-1,2},
-                           {2,-1},{1,-2},{-1,-2},{-2,-1}};
-    if (level == board.length * board[0].length){
-      board[row][col] = level;
-      return true;
-    }
-    for(int[] x: coordinates){
-      try{
-        if (board[row + x[0]][col + x[1]] == 0){
-          board[row][col] = level;
-          if(solveH(row + x[0], col + x[1], level + 1)){
-            return true;
-          }
-          else{
-            board[row][col] = 0;
-          }
-        }
-      }catch(Exception e){
-
-      }
-    }
-    return false;
-  }
-
-  public int countSolutions(int startingRow, int startingCol){
-    for (int i = 0; i < board.length; i++){
-      for (int j = 0; j < board[i].length; j++){
-        if (board[i][j] != 0){
-          throw new IllegalStateException();
-        }
-      }
-    }
-
-    if (startingRow < 0 || startingCol < 0){
-      throw new IllegalArgumentException();
-    }
-    return countHelp(startingRow, startingCol, 1);
-
-  }
-
-  public int countHelp(int row, int col, int level){
-    int sol = 0;
-    int[][] coordinates = {{2,1},{1,2},{-2,1},{-1,2},
-                           {2,-1},{1,-2},{-1,-2},{-2,-1}};
-    if (level == board.length * board[0].length){
-      return 1;
-    }
-    for(int[] x: coordinates){
-      if(row + x[0] < board.length && row + x[0] >= 0 &&
-         col + x[1] < board[0].length && col + x[1] >= 0){
-        if (board[row + x[0]][col + x[1]] == 0){
-          board[row][col] = level;
-          sol += countHelp(row + x[0], col + x[1], level + 1);
-          board[row][col] = 0;
-          }
-      }
-    //  System.out.println(sol);
-    }
-    return sol;
-  }
-
-  public static void main(String[] args){
-    KnightBoard b = new KnightBoard(5,5);
-    System.out.println(b);
-    System.out.println(b.countSolutions(0,0));
-  /*  for (int i = 0; i < 5; i++){
-      for (int j = 0; j < 5; j++){
-        KnightBoard a = new KnightBoard(5,5);
-        System.out.println(a.solve(i,j));
-      }
-    }*/
-
-  }
+	
+public boolean solve(int startingRow, int startingCol){
+	if(startingRow<0||startingCol<0||startingRow>=board.length||startingCol>=board[startingRow].length){
+		throw new IllegalArgumentException();
+	}
+	
+for(int row=0;row<board.length;row++){
+	for(int col=0;col<board[row].length;col++):
+	if(board[row][col]!=0){
+		throw new IllegalStateException();
+	}
 }
-see format for toString below
-blank boards display 0's as underscores 
-you get a blank board if you never called solve or 
-when there is no solution 
+}
+	return solveH(startingRow, startingCol, 1);
+}
 
-@throws IllegalStateException when the board contains non-zero values.
-@throws IllegalArgumentException when either parameter is negative 
- or out of bounds.
-public boolean solve(int startingRow, int startingCol)
+public boolean solveH(int row; int col; int level){
+	if(row<0||row>=board.length||col<0||col>=board[row].length){
+		return false;
+	}
+	if(board[row][col]==0){
+		board[row][col]=level;
+		level++;
+	}
+	else{
+		return false;
+	}
+	for(int i=0; i<ROWMOVES.legnth;i++){
+		if(SolveH(row+ROWMOVES[i],col+COLMOVES[i],level)){
+			return true;
+		}
+	}
+	if(level-1==board.length*board[row].length){
+		return true;
+	}
+	board[row][col]=0;
+	return false;
+}
 
-@throws IllegalStateException when the board contains non-zero values. 
-@throws IllegalArgumentException when either parameter is negative 
- or out of bounds.
-public int countSolutions(int startingRow, int startingCol)
+public int countSolutions(int startingRow,int startingCol){
+	if(startingRow<0||startingCol<0||startingRow>=board.length||startingCol>=board[startingRow].length){
+		throw new IllegalStateException();
+	}
+	for(int row=0;row<board.length;row++){
+		for(int col=0;col<board[row].length;col++){
+			if(board[row][col]!=0){
+				throw new IllegalStateException();
+			}
+		}
+	}
+	return countH(startingRow,startingCol,1);
+}
+public int countH(int row, int col, int level){
+	int ans=0;
+	if(row<0||row>=board.length||col<0||col>=board[row].length){
+		return 0;
+	}
+	if(level-1==board.length*board[row].length){
+		return 1;
+	}
+	if(board[row][col]==0){
+		board[row][col]=level;
+		level++;
+	}
+	else{
+		return 0;
+	}
+	for(int i=0;i<ROWMOVES.length;i++){
+		ans+=countH(row+ROWMOVES[i], col+COLUMNMOVES[i],level);
+	}
+	board[row][col]=0;
+	return ans;
+}
 
-Suggestion:
-private boolean solveH(int row ,int col, int level) 
-// level is the # of the knight
+ 
