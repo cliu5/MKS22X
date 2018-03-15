e5c30506bde5ecf92c827c8c60c69e96576ca9a0
cliu5 <cliu5@stuy.edu>
2018-03-14 20:53:07 -0400

    Update Quick.java

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -9,3 +9,26 @@ public static int quickselect(int[]ary, int k){
 
 
 }
+ public static int partition ( int [] data, int start, int end){
+   
+ int pivotIndex= (int) (Math.random() * (end - start)) + start  ;
+ int val=data[pivotIndex];
+   data[pivotIndex]=data[start];
+   data[start]=data[pivotIndex];
+   int i=start+1;
+   int j=end;
+   while(i<=j){
+     if(data[i]<val){
+          i++;
+     }
+     else{
+       swap(data,i,j);
+       j--;
+     }
+     swap(data,j,start);
+     return j;
+   
+ }
+       
+       
+   
