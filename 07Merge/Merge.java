a3010608c4b6e306174c9f26c57dec1ea350b437
cliu5 <cliu5@stuy.edu>
2018-03-20 15:37:20 -0400

    Update Merge.java

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,22 +1,40 @@
 public class Merge{
+public class Merge{
 public static void mergesort(int[] data){
-//sort the array from least to greatest value. This is a wrapper function that calls msort()//
+    int temp=new int[data.length];
+    msort(data,temp,0,data.length-1);
 }
 
-//Suggestions - have your msort take both the data and temp arrays. Pre-allocate a temp array in your mergesort method//
+
 private static void msort(int[]data, int[]temp, int lo, int hi){
+    if(lo>=hi){
+	return;
+    }
+    else{
+	for(int i=lo;i<=hi;i++){
+	    temp[i]=data[i];
+	}
+	int mid=(lo+hi)/2;
+	msort(temp,data,lo,mid);
+	msort(temp,data,mid+1,hi);
+	merge(data,temp,lo,mid,mid+1,hi);
+    }
+    
+}
+    
+private static void merge(int[]data,int[]temp,int lo, int mid, int hi){
+    
+}
 
-msort:
-  if lo >= hi :  return 
-  copy   data[ lo ] through data[hi]    into    temp[lo] through temp[hi]
-  calculate the midpoint  mid = ???
-  call msort() on each half, but switch the temp and data!
-      msort( ??? ) what parameters are needed?
-      msort( ??? )
-  call merge... which should copy from temp back to data!
-      merge( ???  )  what parameters are needed?
+
+   
+
 
 }
+
+
+ 
+
 private static void merge(int[]data,int[]temp, int[]final){
 }
 }
