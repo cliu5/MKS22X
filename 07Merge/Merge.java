ffcaaef148adbedf3fe44891f7c1d0d1bf6426a5
cliu5 <cliu5@stuy.edu>
2018-03-22 15:28:47 -0400

    Update Merge.java

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,10 +1,9 @@
 public class Merge{
-public class Merge{
+
 public static void mergesort(int[] data){
     int temp=new int[data.length];
     msort(data,temp,0,data.length-1);
 }
-
 
 private static void msort(int[]data, int[]temp, int lo, int hi){
     if(lo>=hi){
@@ -22,10 +21,17 @@ private static void msort(int[]data, int[]temp, int lo, int hi){
     
 }
     
-private static void merge(int[]data,int[]temp,int lo, int mid, int hi){
-    
-}
 
+private static void merge(int[]data,int[]temp,int lo, int mid, int hi){
+    int[]temp=temp[data.length];
+	//copying stuff over//
+	for(int i=lo;i<=mid;i++){
+		temp[i]=data[i];
+	}
+	for(int j=mid+1;j<=hi;j++){
+		temp[mid+1+hi-j]=data[j];
+	}
+	
 
    
 
