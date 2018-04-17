9a892343b64a344507aa6a96d5b4c04bf9f5beeb
cliu5 <cliu5@stuy.edu>
2018-04-16 21:28:32 -0400

    Update Sorts.java

--- 08MyLinkedList/Sorts.java
+++ 08MyLinkedList/Sorts.java
@@ -1,5 +1,32 @@
 public class Sorts{
   public static void radixsort(MyLinkedListImproved<Integer>data){
+    if (data.size()<2){
+      return;
+    }
+   @SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] buckets = new MyLinkedListImproved[10];
+    for(int i=0;i<10;i++){
+      buckets[i]=new MyLinkedListImproved<Integer>();
+    }
+    int max=data.max();
+    int numLoops =0;
+    int temp=max;
+    while(temp>0){
+      temp/10;
+      numLoops++;
+    }
+    for(int i=0;i<numLoops;i++){
+      for(Integer x:data){
+        buckets[digit(i,x)].add(x);
+      }
+      data.clear();
+      for(int x=0;x<10;x++){
+        data.extend(buckets[x]);
+      }
+    }
+  }
+  public static int digit(int i, Integer x){
+    //took this from somewhere on stack overflow bc math is hard :( // 
+    ans = (x / (int)Math.pow(10,i)) % 10
   }
   public static void radixsortIncludingNegatives(MyLinkedListImproved<Integer> data){ 
     //either blank method
