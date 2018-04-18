a9d6c7f3dee6e43ffb4df2fe6862339ed7184649
cliu5 <cliu5@stuy.edu>
2018-04-18 15:25:52 -0400

    Update and rename PostFix.java to Calculator.java

--- 09Stack/Calculator.java
+++ 09Stack/Calculator.java
@@ -1,4 +1,4 @@
-public class PostFix{
+public class Calculator{
 
     public static double eval(String exp){
         Stack S =new Stack();
@@ -49,8 +49,8 @@ public class PostFix{
 	public static void main(String[] args) 
     {
         
-        System.out.println(PostFix.eval("10 2.0 +"));
-	System.out.println(PostFix.eval("11 3 - 4 + 2.5 *"));
-	System.out.println(PostFix.eval("8 2 + 99 9 - * 2 + 9 -"));
+        System.out.println(Calculator.eval("10 2.0 +"));
+	System.out.println(Calculator.eval("11 3 - 4 + 2.5 *"));
+	System.out.println(Calculator.eval("8 2 + 99 9 - * 2 + 9 -"));
     }
 }
