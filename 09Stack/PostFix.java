a791e6950dbe632fef02f19b5c07353f39d82b8f
cliu5 <cliu5@stuy.edu>
2018-04-17 23:20:37 -0400

    Update PostFix.java

--- 09Stack/PostFix.java
+++ 09Stack/PostFix.java
@@ -12,7 +12,7 @@ public class PostFix{
 			
 				S.push(first+second);
 			}
-			if(tokens[i].equals("-")){
+			else if(tokens[i].equals("-")){
 				double first=S.pop();
 			double second=S.pop();
 			
@@ -49,6 +49,8 @@ public class PostFix{
 	public static void main(String[] args) 
     {
         
-        System.out.println(eval("10 2.0 +"));
+        System.out.println(PostFix.eval("10 2.0 +"));
+	System.out.println(PostFix.eval("11 3 - 4 + 2.5 *"));
+	System.out.println(PostFix.eval("8 2 + 99 9 - * 2 + 9 -"));
     }
 }
