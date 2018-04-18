0af7a845e3b94321af96b4124f66816a859c85a5
cliu5 <cliu5@stuy.edu>
2018-04-17 22:18:28 -0400

    Update PostFix.java

--- 09Stack/PostFix.java
+++ 09Stack/PostFix.java
@@ -4,8 +4,8 @@ public class PostFix{
         Stack<Integer> S=new Stack<>();
         for(int i=0;i<exp.length();i++){
             char c=exp.charAt(i);
-            if(Character.isDigit(c+' '))
-            S.push(c);
+            if(Character.isDigit(c))
+            S.push(c+' ');
            
             else
             {
