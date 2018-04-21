3c1fea77e151504510a170c341f0a9a069694571
cliu5 <cliu5@stuy.edu>
2018-04-20 23:11:22 -0400

    Update MyDeque.java
    Finished easy ones

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,10 +1,24 @@
-public class MyDeque{
+public class MyDeque<Type>{
+private int start,end,size;
+private Type[]ary;
 
-MyDeque(){
-}
+  @SuppressWarnings("unchecked")
+    public MyDeque(){
+	ary = (Type[])new Object[10];
+	start = 0;
+	end = 0;
+	size = 10;
+    }
+    
+@SuppressWarnings("unchecked")
 MyDeque(int initialCapacity){
+	array = (Type[])new Object[initialCapacity];
+	start = 0;
+	end = 0;
+	size = initialCapacity;    
 }
 public int size(){
+  return size;
 }
 //These will add the element to the specified side. The deque will double capacity if there is no space left.
 public void addFirst(E){
