5e3a1e2a83dea6a2a7dd5d025bebaff70ac90247
cliu5 <cliu5@stuy.edu>
2018-04-20 23:05:19 -0400

    Create MyDeque.java

---
+++ 10Deque/MyDeque.java
@@ -0,0 +1,24 @@
+public class MyDeque{
+
+MyDeque(){
+}
+MyDeque(int initialCapacity){
+}
+public int size(){
+}
+//These will add the element to the specified side. The deque will double capacity if there is no space left.
+public void addFirst(E){
+}
+public void addLast(E){
+}
+//These will retrieve and remove the element from the specified side.
+public E removeFirst(){
+}
+public E removeLast(){
+}
+//These will retrieve but not remove the element from the specified side.
+public E getFirst(){
+}
+public E getLast(){
+}
+}
