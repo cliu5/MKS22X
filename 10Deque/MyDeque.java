ba59442f59ac32db8944a96ae31af2eb55f554a4
cliu5 <cliu5@stuy.edu>
2018-04-20 23:53:01 -0400

    Update MyDeque.java

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -5,34 +5,57 @@ private Type[]ary;
   @SuppressWarnings("unchecked")
     public MyDeque(){
 	ary = (Type[])new Object[10];
-	start = 0;
+	start = -1;
 	end = 0;
 	size = 10;
     }
     
 @SuppressWarnings("unchecked")
 MyDeque(int initialCapacity){
-	array = (Type[])new Object[initialCapacity];
-	start = 0;
+	ary = (Type[])new Object[initialCapacity];
+	start = -1;
 	end = 0;
 	size = initialCapacity;    
 }
 public int size(){
   return size;
 }
+	//Helpers
+public boolean isFull()
+    {
+        return ((start == 0 && end == size-1)||
+            start == end+1);
+    }
 //These will add the element to the specified side. The deque will double capacity if there is no space left.
-public void addFirst(E){
+public void addFirst(Type element){
+	if(element==null){
+		throw new NullPointerException();
+	}
+	
 }
-public void addLast(E){
+public void addLast(Type element){
 }
 //These will retrieve and remove the element from the specified side.
-public E removeFirst(){
+public Type removeFirst(){
 }
-public E removeLast(){
+public Type removeLast(){
 }
 //These will retrieve but not remove the element from the specified side.
-public E getFirst(){
+	public boolean isEmpty(){
+		return (start == -1);
+	}
+public Type getFirst(){
+	if(isEmpty() || end < 0)
+        {
+            throw new NoSuchElementException();
+        }
+        return ary[start];
 }
-public E getLast(){
+public Type getLast(){
+	if(isEmpty() || end < 0)
+        {
+            throw new NoSuchElementException();
+        }
+        return ary[end];
 }
 }
