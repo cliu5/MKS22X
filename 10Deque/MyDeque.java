99155255eda3e9e073bdd1a0174f5a83c2841291
cliu5 <cliu5@stuy.edu>
2018-04-23 12:38:26 -0400

    Update MyDeque.java

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -6,67 +6,131 @@ private Type[]ary;
   @SuppressWarnings("unchecked")
     public MyDeque(){
 	ary = (Type[])new Object[10];
-	start = -1;
-	end = 0;
-	size = 10;
+	
     }
     
 @SuppressWarnings("unchecked")
-MyDeque(int initialCapacity){
-	ary = (Type[])new Object[initialCapacity];
-	start = -1;
-	end = 0;
-	size = initialCapacity;    
+public MyDeque(int initialCapacity){
+		if(initialCapacity<0){
+throw new IllegalArgumentException();
 }
+	ary = (Type[])new Object[initialCapacity];
+	
+}
+	
+	
 public int size(){
   return size;
 }
+	
+public void addFirst(E element){
+	if(element==null){
+	throw new NullPointerException();
+	}
+	//if its too small//
+else if(size == ary.length){
+resize();
+}
+	//if its not full//
+	else if(size==0){
+	ary[start] = element;
+	}
+	//if start is at the very beginning, add to the end//
+	else if (start == 0){
+		ary[ary.length-1]=element; 
+	start=data.length-1;
+	}
+	//if its just in the middle, the one before it is element//
+	    else{
+      ary[start - 1] =element;
+      start--;
+    }
+    size++;	
+}
+	
+	
+	
+public void addLast(E element){
+	if(element==null){
+	throw new NullPointerException();
+	}
+	
+	//if its too small//
+else if(size == ary.length){
+resize();
+}
+	//if its not full//
+	else if(size==0){
+	ary[end] = element;
+	}
+else if (end == ary.length-1){
+		ary[start]=element; 
+	end=start;
+	}
+	
+else{
+	ary[end+1]=element;
+	end++;
+}
+	size++;
+}
+	
+	
+
 	//Helpers
 public boolean isFull()
     {
         return ((start == 0 && end == size-1)||
             start == end+1);
-    }
-//These will add the element to the specified side. The deque will double capacity if there is no space left.
-	/*
-public void addFirst(Type element){
-	if(element==null){
-		throw new NullPointerException();
+}
+
+	
+	
+	
+	
+//These will retrieve and remove the element from the specified side.
+public Type removeLast(){
+	 if(isEmpty()||end<0){
+		throw new NoSuchElementException();
 	}
 
-        if (isFull())
-        {
-            resize();
-	    }
- 
-        if (start == -1)
-        {
-            start = 0;
-            end = 0;
+        Type ans = data[end];
+        data[end] = null;
+        if (end == 0) {
+            end = data.length - 1;
         }
-         
-        // front is at first position of queue
-        else if (start == 0)
-            start = size - 1 ;
-  
-        else // decrement front end by '1'
-            start = start-1;
-  
-        // insert current element into Deque
-        ary[start] = element ;
+
+        else {
+            end--;
+        }
+
+        size--;
+        return ans;
     }
 	
 	
 	
-}
-*/
-public void addLast(Type element){
-}
-//These will retrieve and remove the element from the specified side.
+	
+	
+
 public Type removeFirst(){
+	if(isEmpty()||end<0){
+		throw new NoSuchElementException();
+	}
+	 Type ans = ary[start];
+        ary[start] = null;
+        if (start == ary.length - 1) {
+            start = 0;
+        }
+        else {
+            start++;
+        }
+
+        size--;
+        return ans;
+		
 }
-public Type removeLast(){
-}
+
 //These will retrieve but not remove the element from the specified side.
 	public boolean isEmpty(){
 		return (start == -1);
