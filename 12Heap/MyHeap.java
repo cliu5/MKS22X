91cb7fe239c6f4c9750c369de7b2b524f476a23e
cliu5 <cliu5@stuy.edu>
2018-05-02 23:51:17 -0400

    skeleton done

--- 12Heap/MyHeap.java
+++ 12Heap/MyHeap.java
@@ -1,5 +1,22 @@
 public class MyHeap{
+  public void MyHeap(){
+  }
+  
+  //construct empty max heap//
 
+  public void MyHeap(boolean){
 
+  }
+ //true ; construct empty max heap, false: construct empty min heap.
+  
+  public void add(String s){
+  }
+  public String remove(){
+  }
+  public String peek(){
+  }
+public int size(){
+}
+  
 
 }
