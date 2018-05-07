a882e315b380e0dee49327f28be43464c47a4de2
cliu5 <cliu5@stuy.edu>
2018-05-06 22:32:45 -0400

    Update Sorts.java

--- 12Heap/Sorts.java
+++ 12Heap/Sorts.java
@@ -2,14 +2,14 @@ import java.util.*;
 public class Sorts{
     
     public static void heapsort(Integer[] ary){
-	MyHeap<int> heap = heapify(ary);
+	MyHeap<Integer> heap = heapify(ary);
 	for ( int i = ary.length-1 ; i>0;i--){
     ary[i]=heap.remove();
     }
     }
     
     public static MyHeap<Integer> heapify(Integer[] ary){
-	MyHeap<int> heap = new MyHeap<>();
+	MyHeap<Integer> heap = new MyHeap<>();
 	for(int i = 0; i < ary.length; i++){
 	    heap.add(ary[i]);
 	}
