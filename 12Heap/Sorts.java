98103366aa7001efc919d2f9276fa250c1984208
cliu5 <cliu5@stuy.edu>
2018-05-06 22:32:28 -0400

    Update Sorts.java

--- 12Heap/Sorts.java
+++ 12Heap/Sorts.java
@@ -19,28 +19,4 @@ public class Sorts{
  
 
   
-    
-   
-    public static void main(String[] args){
-    Integer[] reg = new int[25];
-    Integer[] heap = new int[25];
-
-    for(int i = 0; i < 25; i ++){
-      int temp = (int)(Math.random() * 1000);
-      reg[i] = temp;
-      heap[i] = temp;
-    }
-
-    Arrays.sort(reg);
-    Sorts.heapsort(heap);
-
-    for(int i = 0; i < 25; i++){
-      if(reg[i] != heap[i]){
-        System.out.println("There is an error at index " + i);
-        System.out.println("reg: " + Arrays.toString(reg));
-        System.out.println("hea: " + Arrays.toString(heap));
-      }
-    }
-  }
-    
 }
