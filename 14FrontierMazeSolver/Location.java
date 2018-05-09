cb241d3e17d00074a2c3564810372781e6ce75b6
cliu5 <cliu5@stuy.edu>
2018-05-08 23:05:40 -0400

    Update and rename Location to Location.java

--- 14FrontierMazeSolver/Location.java
+++ 14FrontierMazeSolver/Location.java
@@ -4,6 +4,9 @@ public class Location{
     private int x,y;
     private Location previous;
     public Location(int _x, int _y, Location prev){
+    x=_x;
+    y=_y;
+    previous=prev;
     }
     
     
