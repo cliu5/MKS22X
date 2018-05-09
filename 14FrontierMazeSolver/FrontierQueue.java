c2cbec53d416f47f7deecab453dad73d91f22f9a
cliu5 <cliu5@stuy.edu>
2018-05-08 22:54:07 -0400

    Create FrontierQueue.java

---
+++ 14FrontierMazeSolver/FrontierQueue.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class FrontierQueue implements Frontier{
+  private ArrayList<Location> f;
+
+  public FrontierQueue(){
+    f = new LinkedList<>();
+  }
+
+  public Location next(){
+    return f.removeFirst();
+  }
+
+  public void add(Location n){
+    f.add(n);
+  }
+
+  public boolean hasNext(){
+    return f.size() != 0;
+  }
+
+}
