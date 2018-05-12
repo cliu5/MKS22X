dd7ad302ce6673323c2a6c01b4c0f91880ba6db0
cliu5 <cliu5@stuy.edu>
2018-05-12 15:51:51 -0400

    Update Sorts.java

--- 12Heap/Sorts.java
+++ 12Heap/Sorts.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class Sorts{
+	//USED BRITNI'S DRIVER//
     
 public static void heapsort(Integer[] ary){
 
@@ -14,26 +15,24 @@ public static void heapsort(Integer[] ary){
     }
     
     
- public static void main(String[] args){
-    Integer[] reg = new Integer[25];
-    Integer[] heap = new Integer[25];
-
-    for(int i = 0; i < 25; i ++){
-      int temp = (int)(Math.random() * 1000);
-      reg[i] = temp;
-      heap[i] = temp;
+  public static void main(String[] args){
+	Integer[] A = new Integer[Integer.parseInt(args[0])];
+	Integer[] B = new Integer[A.length];
+	Random r = new Random();
+	for(int i = 0; i < A.length; i++){
+	    Integer n = Integer.valueOf(r.nextInt(10000));
+	    B[i] = n;
+	    A[i] = n;
+	}
+	System.out.println(Arrays.toString(A));
+	heapsort(A);
+	Arrays.sort(B);
+	for(int i = 0; i < A.length; i++){
+	    if(!A[i].equals(B[i])){
+		System.out.println("ERROR");
+	    }
+	}
+	System.out.println(Arrays.toString(A));
     }
 
-    Arrays.sort(reg);
-    Sorts.heapsort(heap);
-
-    for(int i = 0; i < 25; i++){
-      if(reg[i] != heap[i]){
-        System.out.println("There is an error at index " + i);
-        System.out.println("reg: " + Arrays.toString(reg));
-        System.out.println("hea: " + Arrays.toString(heap));
-}
-  
-}
- }
 }
