ba48cbeae35745e375066c1c7040109eed441b4e
cliu5 <cliu5@stuy.edu>
2018-05-13 19:01:09 -0400

    Update MazeSolver.java

--- 14FrontierMazeSolver/MazeSolver.java
+++ 14FrontierMazeSolver/MazeSolver.java
@@ -10,14 +10,40 @@ public class MazeSolver{
   }
   
   public boolean solve(int mode){
-    
-    
-    
-    
-    
-    //haha kill me//
-    return false;
-  }
+
+    public boolean solve(int mode){
+	
+	if(mode==0){
+	    frontier = new FrontierQueue();
+	}else if(mode==1){
+	    frontier = new FrontierStack();
+	}else{
+	    frontier = new FrontierPriorityQueue();
+	}
+	
+	frontier.add(maze.getStart());
+	while(frontier.hasNext()){
+	    Location l = frontier.next();
+    maze[l.getX()][l.getY()]='.';
+	   Location[] neighbors = maze.getNeighbors(l);
+	
+    //CHECKING FOR END//
+	    for(int i = 0; i<neighbors.length && neighbors[i]!=null; i++){
+		    if(maze.getEnd().getX()==neighbors[i].getX() &&
+		       maze.getEnd().getY()==neighbors[i].getY()){
+		
+			Location _l = neighbors[i].getPrev();
+			while(_l.getX()!=maze.getStart().getX() || _l.getY()!=maze.getStart().getY()){
+        maze[_l.getX()][_l.getY()]['@'];
+			    _l = _l.getPrev();
+			}
+          maze[maze.getStart().getX()]maze.getStart().getY()]='@';
+			return true;
+		    }
+		    frontier.add(neighbors[i]);
+	}
+	return false;
+    }
   
   public String toString(){
     return maze.toString();
