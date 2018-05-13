fe9126eb0a20863ffe55ca31459a723a7ff0d55c
cliu5 <cliu5@stuy.edu>
2018-05-13 16:04:29 -0400

    Create FrontierPriorityQueue.java

---
+++ 14FrontierMazeSolver/FrontierPriorityQueue.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class FrontierPriorityQueue implements Frontier{
+
+    private MyHeap<Location> data;
+
+    public FrontierPriorityQueue(){
+	data = new MyHeap<>(false);
+    }
+
+    public void add(Location l){
+	data.add(l);
+    }
+    
+     public Location next(){
+    if(data.size() == 0){
+      throw new NoSuchElementException();
+    }
+    return data.remove();
+  }
+
+    public boolean hasNext(){
+	return data.size()>0;
+    }
+}
