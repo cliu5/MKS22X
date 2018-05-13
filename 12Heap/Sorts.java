e2336bf4214b3ce4bb618c19e00823cca0f2e0c2
cliu5 <cliu5@stuy.edu>
2018-05-12 22:15:04 -0400

    Update Sorts.java

--- 12Heap/Sorts.java
+++ 12Heap/Sorts.java
@@ -1,6 +1,5 @@
 import java.util.*;
 public class Sorts{
-	//USED BRITNI'S DRIVER//
     
 public static void heapsort(Integer[] ary){
 
@@ -15,24 +14,6 @@ public static void heapsort(Integer[] ary){
     }
     
     
-  public static void main(String[] args){
-	Integer[] A = new Integer[Integer.parseInt(args[0])];
-	Integer[] B = new Integer[A.length];
-	Random r = new Random();
-	for(int i = 0; i < A.length; i++){
-	    Integer n = Integer.valueOf(r.nextInt(10000));
-	    B[i] = n;
-	    A[i] = n;
-	}
-	System.out.println(Arrays.toString(A));
-	heapsort(A);
-	Arrays.sort(B);
-	for(int i = 0; i < A.length; i++){
-	    if(!A[i].equals(B[i])){
-		System.out.println("ERROR");
-	    }
-	}
-	System.out.println(Arrays.toString(A));
-    }
+ 
 
 }
