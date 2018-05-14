18be82b7840168655596c2593c4ca083e7b883a8
cliu5 <cliu5@stuy.edu>
2018-05-13 21:23:51 -0400

    Update MazeSolver.java

--- 14FrontierMazeSolver/MazeSolver.java
+++ 14FrontierMazeSolver/MazeSolver.java
@@ -69,7 +69,7 @@ private boolean animate;
     return maze.toString();
   }
 	     public static void main(String[] args){
-	MazeSolver test = new MazeSolver("data2.dat");
+	MazeSolver test = new MazeSolver("data3.dat");
 
 	System.out.println(test.solve(2));
     }
