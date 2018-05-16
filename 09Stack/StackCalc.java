6786ac572d121409afe5dab2be3b9f684f76da31
cliu5 <cliu5@stuy.edu>
2018-05-16 15:11:30 -0400

    Update and rename Calculator.java to StackCalc.java

--- 09Stack/StackCalc.java
+++ 09Stack/StackCalc.java
@@ -1,4 +1,4 @@
-public class Calculator{
+public class StackCalc{
 
     public static double eval(String exp){
         Stack S =new Stack();
@@ -46,11 +46,5 @@ public class Calculator{
 	    
     }
    
-	public static void main(String[] args) 
-    {
-        
-        System.out.println(Calculator.eval("10 2.0 +"));
-	System.out.println(Calculator.eval("11 3 - 4 + 2.5 *"));
-	System.out.println(Calculator.eval("8 2 + 99 9 - * 2 + 9 -"));
-    }
+	
 }
