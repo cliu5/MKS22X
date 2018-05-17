9f08b2d4b0b649e09f3d4143e16727b5c3fc3ec9
cliu5 <cliu5@stuy.edu>
2018-05-16 22:41:43 -0400

    Update USACODriver.java

--- 05USACO/USACODriver.java
+++ 05USACO/USACODriver.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class USACODriver{
+public class USACODriver throws FileNotFoundException{
 
     public static void main(String[] args){
 	for (int i = 1; i < 6; i ++){
