91caa8c31963140b6e10a1f92f1f0af8524af3ad
cliu5 <cliu5@stuy.edu>
2018-05-19 20:26:55 -0400

    Update MyHeap.java

--- 12Heap/MyHeap.java
+++ 12Heap/MyHeap.java
@@ -137,21 +137,5 @@ public void swap(T[]ary, int first, int second){
 	}
     }
 
-	public static void main(String[] args){
-	MyHeap<Integer> heap = new MyHeap<>();
-	for(int i = 0; i < 15; i++){
-	    heap.add((int)(i*Math.random()*10));
-	}
-	System.out.println(heap);
-	heap.remove();
-	System.out.println(heap);
-
-	heap.remove();
-	System.out.println(heap);
-
-	heap.remove();
-
-	System.out.println(heap);
-    }
 	
 }
