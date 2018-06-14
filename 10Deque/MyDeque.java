e2e2ece1de5105db59b61cd9fc919dfb62e066d5
cliu5 <cliu5@stuy.edu>
2018-06-14 17:04:52 -0400

    Update MyDeque.java

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,15 +1,12 @@
 import java.util.*;
+
 public class MyDeque<Type>{
 private int start,end,size;
 private Type[]data;
 
-  @SuppressWarnings("unchecked")
+  
     public MyDeque(){
-data=(Type[]) new Object[10]; 
-      start=0;
-      end=-1;
-      size=0;
-      
+this(10);
     }
     
 @SuppressWarnings("unchecked")
@@ -19,7 +16,7 @@ throw new IllegalArgumentException();
 }
 data = (Type[])new Object[initialCapacity];
   start=0;
-  end=-1;
+  end=0;
   size=0;
 }
 public int size(){
@@ -28,7 +25,7 @@ public int size(){
   
   
 public void addFirst(Type element){
-if(element==(null)){
+if(element==null){
 throw new NullPointerException();
 }
  
@@ -36,75 +33,71 @@ throw new NullPointerException();
  if(size == data.length){
 resize();
 }
-  if(start==0){
-    start=data.length;
-  }
-  start--;
-  
-      if(data[start]==null){
-         data[start]=element;
+      if (size != 0){
+        start = (start-1+data.length) % data.length;
+    }
+    data[start] = element;
     size++;
   }
-}
 
-public void addLast(Type element){
+  
+}
+  
+  
+ public void addLast(Type element){
 if(element==null){
 throw new NullPointerException();
 }
-  if((data.length)==size()){
+  if(size==data.length){
     resize();
   }
-  if (end==size()){
-   end=-1;
-  }
-  end++;
-if(data[end]==null){
-data[(end)]=element;
+if(size!=0){
+end=(end+1)%data.length;
+data[end]=element;
 size++;
 }
-}
+} 
   
-
-//Helpers
-public boolean isFull()
+  
+  public boolean isFull()
     {
         return ((start == 0 && end == size-1)||
             start == end+1);
 }
-
-//These will retrieve and remove the element from the specified side.
-public Type removeLast(){
+  
+  
+  
+  public Type removeLast(){
 if(size<=0){
 throw new NoSuchElementException();
 }
 
         Type ans = data[end];
         data[end] = null;
-        
-            end = (end-1+data.length)%data.length;
-        
-        end--;
-        return ans;
-    }
+if(size>1){
+   end = (end-1+data.length)%data.length;
+        }
+size—;
+return ans;
+}
 
+  
 public Type removeFirst(){
-if(size()<=0){
+if(size<=0){
 throw new NoSuchElementException();
 }
 Type ans = data[start];
         data[start] = null;
-       
+       if(size>1){
             start = (start+1)%data.length;
-        
+       }
         size--;
         return ans;
 }
-
-//These will retrieve but not remove the element from the specified side.
-public boolean isEmpty(){
-return (start == -1);
-}
-public Type getFirst(){
+  
+  
+  
+  public Type getFirst(){
 if(size<=0)
         {
             throw new NoSuchElementException();
@@ -118,15 +111,23 @@ if(size<=0)
         }
         return data[end];
 }
+  
+  
+  
+  
+  public boolean isEmpty(){
+return (start == -1);
+}
+
 @SuppressWarnings("unchecked")
 //copy into ans
-private void resize(){
-Type[] ans = (Type[])new Object[data.length * 2 +1];
+public void resize(){
+Type[] ans = (Type[])new Object[size * 2 +1];
 for (int i = 0; i < data.length; i++){
    ans[i] = data[(start + i) % data.length];
 }
 start = 0;
-end = data.length-1;
+end = size-1;
 data = ans;
     }
 
