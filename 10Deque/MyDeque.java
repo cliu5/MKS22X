22980048a81147dcff5ce88e3f8497028b6d05f8
cliu5 <cliu5@stuy.edu>
2018-06-14 17:06:36 -0400

    Update MyDeque.java

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+// USED CRYSTAL'S BEUATIFUL LONG DRIVER// 
+//THANKS CRYSTAL!!//
 public class MyDeque<Type>{
 private int start,end,size;
 private Type[]data;
@@ -130,5 +132,51 @@ start = 0;
 end = size-1;
 data = ans;
     }
+   public static void main(String[] args) {
+    MyDeque<String> a = new MyDeque<>(), a1 = new MyDeque<>();
+    ArrayList<String> b = new ArrayList<>();
+
+    int size = Integer.parseInt(args[0]);
+    for(int i = 0; i < size; i++){
+      int temp = (int)(Math.random() * 1000);
+      if(temp % 2 == 0){
+        a.addFirst("" + temp);
+        a1.addFirst("" + temp);
+        b.add(0, "" + temp);
+      }
+      else{
+        a.addLast("" + temp);
+        a1.addLast("" + temp);
+        b.add("" + temp);
+      }
+    }
+
+    int index = 0;
+    boolean hasError = false;
+    String errorEvaluation = "Errors found at these indices: ";
+    for (String x : b){
+      if (!(x.equals(a.getFirst()))){
+        System.out.println("The getFirst() function is incorrect at index " + index);
+        hasError = true;
+      }
+      if (!(x.equals(a.removeFirst()))){
+        System.out.println("There is an error at index " + index);
+        errorEvaluation += index + ", ";
+        hasError = true;
+      }
+      index++;
+    }
+
+
+    if(hasError){
+      errorEvaluation = errorEvaluation.substring(0, errorEvaluation.length() - 2);
+      System.out.println(errorEvaluation);
+      System.out.println("MyDeque: " + a1);
+      System.out.println("Actual Deque: " + b);
+    }
+    else{
+      System.out.println("Your deque is bug-free!");
+    }
+  }
 
 }
