4d58fa2986ea5f02c4355c0fcf8aac798f150771
cliu5 <cliu5@stuy.edu>
2018-06-15 00:04:17 -0400

    Update Sorts.java

--- 12Heap/Sorts.java
+++ 12Heap/Sorts.java
@@ -24,37 +24,5 @@ public class Sorts{
 	ary[second]=temp;
 }
     
-    public static int size(Integer[] array){
-		int ans = 0;
-	for(int i = 0; i < array.length; i++){
-	    if(array[i] != null){
-		ans++;
-	    }
-	}
-	return ans;
-
-    }
-	  public static String print(Integer[] array){
-	String returnString = "[";
-	for(int i = 0; i < array.length; i++){
-	    returnString = returnString + array[i]  +", ";
-	}
-	returnString = returnString.substring(0, returnString.length() - 2);
-	return returnString + "]";
-    }
     
-    public static void main(String[] args){
-	Integer[] a = new Integer[10];
-
-	
-	for(int i = 0; i < 10; i++){
-	    a[i] = new Integer ((int)(Math.random() * 10));
-	}
-
-	System.out.println("....." + print(a));
-	heapsort(a);
-	System.out.println("....." + print(a));
-	
-    }
-
 }
