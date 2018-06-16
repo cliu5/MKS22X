48faa96d8817f64ae0fef3967c6f00cc1686a424
cliu5 <cliu5@stuy.edu>
2018-06-16 17:09:29 -0400

    Update RunningMedian.java

--- 13Medians/RunningMedian.java
+++ 13Medians/RunningMedian.java
@@ -49,6 +49,27 @@ if (maxHeap.size() > minHeap.size()){
   public int size(){
 	  return maxHeap.size()+minHeap.size();
   }
+public static void main(String[] args){
+	
+	RunningMedian RM = new RunningMedian();
+	/*
+	RM.add(1.0);
+	RM.add(5.0);
+	RM.add(8.0);
+	RM.add(3.0);
+	RM.add(2.0);
+	RM.add(1.0);
+	System.out.println(RM);
+	System.out.println(RM.getMedian());
+	*/
+	for(int i = 0; i < Integer.parseInt(args[0]); i++){
+	    double temp = (int)(Math.random() * 1000);
+	    RM.add(temp);
+	}
+	System.out.println(RM.getMedian());
+	System.out.println(RM);
+	
+    }
 	
 	    
 }
