8df02f21f2fd60e6bf5271b2bf62ed972347141b
cliu5 <cliu5@stuy.edu>
2018-06-16 16:49:27 -0400

    Update RunningMedian.java

--- 13Medians/RunningMedian.java
+++ 13Medians/RunningMedian.java
@@ -1,32 +1,22 @@
 import java.util.*;
 public class RunningMedian{
 
-    MyHeap<Double> maxHeap;
-    MyHeap<Double> minHeap;
-    int size;
+    public MyHeap<Double> maxHeap;
+    public MyHeap<Double> minHeap;
 
+ @SuppressWarnings("unchecked")
     public RunningMedian(){
-	
-	minHeap = new MyHeap<>();
-	maxHeap = new MyHeap(false);
-	size=0;
-
+	minHeap = new MyHeap<>(false);
+	maxHeap = new MyHeap();
     }
 	
-	 public RunningMedian(double[] a){
-    minHeap = new MyHeap<>();
-    maxHeap = new MyHeap<>(false);
-    for(int i = 0; i < a.length; i++){
-      add(a[i]);
-    }
-  }
-
+	
     public void add(Double a){
 	    if(size() == 0){
 	    maxHeap.add(a);
 	    }
 	
-	 else if (a>=getMedian()){
+	 else if (a>getMedian()){
 		 minHeap.add(a);
 	 }
 	 else{
@@ -38,40 +28,27 @@ public class RunningMedian{
 	if(minHeap.size()>maxHeap.size()+1){
 		maxHeap.add(minHeap.remove());
 	} 
-	    size++;
     }
 
     public double getMedian(){
 	if(size()==0){
 		throw new NoSuchElementException();
 	}
-	if(maxHeap.size() == minHeap.size()){
-	    return (maxHeap.peek() + minHeap.peek()) / 2.0;
-	}
-	else{
-	    if (maxHeap.size() > minHeap.size()){
+if (maxHeap.size() > minHeap.size()){
 		return maxHeap.peek();
 	    }
-	    else{
+	    else if (minHeap.size()>maxHeap.size()){
 		return minHeap.peek();
 	    }
+	    else{
+	    return (maxHeap.peek() + minHeap.peek()) / 2.0;
 	}
+}
     }
 
   public int size(){
-	  return size;
+	  return maxHeap.size()+minHeap.size();
   }
 	
-	    public static void main(String[] args){
-    double[] data = {1.0,3.0,5.0,4.0,2.0};
-    RunningMedian a = new RunningMedian(data);
-    System.out.println(a.getMedian()); //3
-//    System.out.println(a.minHeap);
-//    System.out.println(a.maxHeap);
-    a.add(6.0);
-    System.out.println(a.getMedian()); //3.5
-    a.add(1.0);
-    a.add(1.0);
-    System.out.println(a.getMedian());//2.5
-  }
+	    
 }
