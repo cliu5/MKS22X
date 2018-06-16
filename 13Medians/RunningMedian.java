27ae1a74208dc1476c95bf076794b9d985094bd1
cliu5 <cliu5@stuy.edu>
2018-06-16 16:15:49 -0400

    Update RunningMedian.java

--- 13Medians/RunningMedian.java
+++ 13Medians/RunningMedian.java
@@ -2,72 +2,45 @@ public class RunningMedian{
 
     MyHeap<Double> maxHeap;
     MyHeap<Double> minHeap;
-    int maxSize;
-    int minSize;
-    int choose;
+    int size;
 
     public RunningMedian(){
 	
-	minHeap = new MyHeap<>();
-	maxHeap = new MyHeap(false);
-	maxSize = 0;
-	minSize = 0;
-	choose = 0;
+	minHeap = new MyHeap<>(false);
+	maxHeap = new MyHeap();
+	size=0;
 
     }
 
-    public void add(Double n){
-	if (n > this.getMedian()){
-	    minHeap.add(n);
-	    minSize += 1;
-	}
-	else{
-	    if(n < this.getMedian()){
-		maxHeap.add(n);			
-		maxSize += 1;
+    public void add(Double a){
+	    if(size() == 0){
+	    maxHeap.add(a);
 	    }
-	    else{
-		if (choose % 2 == 0){
-		    maxHeap.add(n);
-		    maxSize += 1;
-
-		}
-		else{
-		    minHeap.add(n);
-		    minSize += 1;
-
-		}
-		choose += 1;
+	
+	 else if (a>=getMedian()){
+		 minHeap.add(a);
+	 }
+	 else{
+		maxHeap.add(element);
 	    }
+	if(maxHeap.size()>minHeap.size()+1){
+		minHeap.add(maxHeap.remove());
 	}
-
-	//have to do the balancing thingymabob
-	if (maxSize > minSize + 1){
-	    minHeap.add(maxHeap.remove());
-	    minSize += 1;
-	    maxSize -= 1;
-	}
-
-	if (minSize > maxSize + 1){
-	    maxHeap.add(minHeap.remove());
-	    minSize -= 1;
-	    maxSize += 1;
-	}
-	  
-
+	if(minHeap.size()>maxHeap.size()+1){
+		maxHeap.add(minHeap.remove());
+	} 
+	    size++;
     }
 
     public double getMedian(){
-	
-	if(maxSize == 0 && minSize == 0){
-	    return 0;
+	if(size()==0){
+		throw new NoSuchElementException();
 	}
-	
-	if(maxSize == minSize){
+	if(maxHeap.size() == minHeap.size()){
 	    return (maxHeap.peek() + minHeap.peek()) / 2.0;
 	}
 	else{
-	    if (maxSize > minSize){
+	    if (maxHeap.size() > minHeap.size()){
 		return maxHeap.peek();
 	    }
 	    else{
@@ -77,18 +50,22 @@ public class RunningMedian{
     }
 
   public int size(){
-	  return minSize+maxSize;
+	  return size;
   }
-	public static void main(String[] args){
-    double[] data = {1.0,3.0,5.0,4.0,2.0};
-    RunningMedian a = new RunningMedian(data);
-    System.out.println(a.getMedian()); //3
-//    System.out.println(a.minHeap);
-//    System.out.println(a.maxHeap);
-    a.add(6.0);
-    System.out.println(a.getMedian()); //3.5
-    a.add(1.0);
-    a.add(1.0);
-    System.out.println(a.getMedian());//2.5
-  }
+	
+	    public static void main(String[] args){
+	RunningMedian a = new RunningMedian();
+	//System.out.println(a);
+	a.add(1230.0);
+	a.add(320.0);
+	a.add(440.0);
+	a.add(9560.0);
+	a.add(80.0);
+	a.add(100.0);
+	a.add(442.0);
+	a.add(12398120938.021312443);
+	System.out.println("med: " + a.getMedian());
+	System.out.println(a);
+	System.out.println(a.size());
+    }
 }
